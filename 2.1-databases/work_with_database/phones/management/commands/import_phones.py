1752ab4cb2ad4cadba855d8c8cb1aba6f9eb0b4a
Philip Sosnin <fff26@mail.ru>
2023-10-02 08:04:34 +0400

    refining and moving code from the application dir

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,25 @@
 import csv
-
+from slugify import slugify
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('phones.csv', type=str, help='Path to the CSV file')
 
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            phone = Phone(
+                id = phone['id'],
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=slugify(phone['name']),
+            )
+            phone.save()
