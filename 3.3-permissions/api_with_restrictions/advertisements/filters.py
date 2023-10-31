c01c9fc66449108eb1af23be68fd51e72c7da238
Philip Sosnin <fff26@mail.ru>
2023-10-31 09:56:09 +0400

    After revision 31_10_23

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,13 +1,22 @@
 from django_filters import rest_framework as filters
-
+from advertisements.models import AdvertisementStatusChoices
 from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
-
     created_at_before = filters.DateFromToRangeFilter()
+    status = filters.ChoiceFilter(
+        method="filter_status"
+    )
+
+    def filter_status(self, queryset, name, value):
+        if value == AdvertisementStatusChoices.OPEN:
+            return queryset.filter(status=AdvertisementStatusChoices.OPEN)
+        elif value == AdvertisementStatusChoices.CLOSED:
+            return queryset.filter(status=AdvertisementStatusChoices.CLOSED)
+        return queryset
 
     class Meta:
         model = Advertisement
-        fields = ["created_at"]
+        fields = ["created_at", "status"]
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,10 +1,11 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.permissions import IsAuthenticated, BasePermission, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
+
+from rest_framework import permissions
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -12,16 +13,22 @@ class AdvertisementViewSet(ModelViewSet):
 
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    filter_backends = [DjangoFilterBackend]
     filter_class = AdvertisementFilter
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated()]
         return []
+
+    def perform_create(self, serializer):
+        """Создание объявления."""
+        serializer.save(creator=self.request.user)
 
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return obj.creator == request.user
